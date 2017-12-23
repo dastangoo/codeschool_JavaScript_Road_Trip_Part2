c0c9211f8a8a4e6e56d4f6dd1e892c30d31c4596
dastangoo <hamed.dastangoo@gmail.com>
2017-12-23 15:19:29 +0330

    More Loops

--- trains.js
+++ trains.js
@@ -1,6 +1,20 @@
-var trainsOperational = 8;
 var trainNumber = 1;
+var trainsOperational = 8;
+var totalTrains = 12;
+
+// for (var trainNumber = 1; trainNumber < trainsOperational; trainNumber++) {
+//   console.log("Train #" + trainNumber + " is running.");
+// }
+
+// for (var number = 5; number > 0; number--) {
+//   console.log(number);
+// }
+
+
 while (trainNumber <= trainsOperational) {
   console.log("Train #" + trainNumber + " is running.");
   trainNumber++;
+}
+for (var stoppedTrain = trainsOperational + 1; stoppedTrain <= totalTrains; stoppedTrain++) {
+  console.log("Train #" + stoppedTrain + " is not operational.");
 }
