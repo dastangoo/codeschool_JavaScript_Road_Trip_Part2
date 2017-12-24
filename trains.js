8911f6f4909c2e03148f6de52b91c93775e35510
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 09:39:16 +0330

    Loop Slide

--- trains.js
+++ trains.js
@@ -2,19 +2,23 @@ var trainNumber = 1;
 var trainsOperational = 8;
 var totalTrains = 12;
 
-// for (var trainNumber = 1; trainNumber < trainsOperational; trainNumber++) {
-//   console.log("Train #" + trainNumber + " is running.");
-// }
+for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
+  if (trainNumber <= trainsOperational) {
+    console.log("Train #" + trainNumber + " is running.");
+  } else {
+      console.log("Train #" + trainNumber + " is not operational.");
+  }
+}
 
 // for (var number = 5; number > 0; number--) {
 //   console.log(number);
 // }
 
 
-while (trainNumber <= trainsOperational) {
-  console.log("Train #" + trainNumber + " is running.");
-  trainNumber++;
-}
-for (var stoppedTrain = trainsOperational + 1; stoppedTrain <= totalTrains; stoppedTrain++) {
-  console.log("Train #" + stoppedTrain + " is not operational.");
-}
+// while (trainNumber <= trainsOperational) {
+//   console.log("Train #" + trainNumber + " is running.");
+//   trainNumber++;
+// }
+// for (var stoppedTrain = trainsOperational + 1; stoppedTrain <= totalTrains; stoppedTrain++) {
+//   console.log("Train #" + stoppedTrain + " is not operational.");
+// }
