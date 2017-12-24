0f4b2657f0e7a861ddde78ebcd9acc295a4fc8e4
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 13:26:00 +0330

    Array Ho!

--- trains.js
+++ trains.js
@@ -39,11 +39,48 @@
 // }
 
 
-var gotName = false;
-while (gotName == false) {
-  var userName = prompt("Yo passenger! What's your name?");
-  if (confirm("Are you sure your name is " + userName + "?")) {
-    alert("'Sup " + userName + "!");
-    gotName = true;
-  }
+// var gotName = false;
+// while (gotName == false) {
+//   var userName = prompt("Yo passenger! What's your name?");
+//   if (confirm("Are you sure your name is " + userName + "?")) {
+//     alert("'Sup " + userName + "!");
+//     gotName = true;
+//   }
+// }
+
+
+function makeList() {
+  
 }
+
+
+var arr = ['a', 'b', 'c'];
+console.log(arr);
+
+arr.push('d');
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.unshift('a');
+console.log(arr);
+
+var comboArray1 = ["One", "fish", 2, "fish"];
+console.log(comboArray1);
+
+var poisson = "fish";
+console.log(poisson);
+
+var comboArray2 = ["One", poisson, "Blue", poisson]
+console.log(comboArray2);
+
+var arrayOfArrays = [comboArray1, comboArray2];
+console.log(arrayOfArrays);
+
+console.log(arrayOfArrays[1]);
+console.log(arrayOfArrays[1][0]);
+console.log(arrayOfArrays[0][1]);
