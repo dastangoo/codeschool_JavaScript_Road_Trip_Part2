b63455841cf5887821a90e3f0ade89fcbf72e5b6
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 09:51:35 +0330

    A Loopy Problem

--- trains.js
+++ trains.js
@@ -2,15 +2,24 @@ var trainNumber = 1;
 var trainsOperational = 8;
 var totalTrains = 12;
 
-for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
-  if (trainNumber <= trainsOperational) {
-    console.log("Train #" + trainNumber + " is running.");
-  } else if(trainNumber == 10) {
-    console.log("Train #10 begins running at noon.");
+
+if (trainsOperational > 0) {
+  if (trainsOperational == totalTrains) {
+    console.log("All trains are running at the JavaScript Express!");    
+  } else {
+    for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
+      if (trainNumber <= trainsOperational) {
+        console.log("Train #" + trainNumber + " is running.");
+      } else if(trainNumber == 10) {
+        console.log("Train #10 begins running at noon.");
+      }
+      else {
+          console.log("Train #" + trainNumber + " is not operational.");
+      }
+    }      
   }
-  else {
-      console.log("Train #" + trainNumber + " is not operational.");
-  }
+} else {
+  console.log("No trains are operational today. Bummer!");
 }
 
 // for (var number = 5; number > 0; number--) {
