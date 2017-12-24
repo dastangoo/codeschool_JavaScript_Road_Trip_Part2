a9379da07a76a05c7c33a31c695c46411974c176
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 13:06:45 +0330

    Problem Solving with Functions

---
+++ example05.js
@@ -0,0 +1,17 @@
+var x = 6;
+var y = 4;
+
+function add(a, b) {
+  var x = a + b;
+  return x;
+}
+
+function subtract(a, b) {
+  y = a - b;
+  return y;
+}
+
+console.log(add(1, 2));
+console.log(x);
+console.log(subtract(2, 1));
+console.log(y);
