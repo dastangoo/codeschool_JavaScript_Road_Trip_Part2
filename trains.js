2c17a4313f17ca2d24cc5c0b47dbd63b88e67c53
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 09:46:55 +0330

    Using else if

--- trains.js
+++ trains.js
@@ -5,7 +5,10 @@ var totalTrains = 12;
 for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
   if (trainNumber <= trainsOperational) {
     console.log("Train #" + trainNumber + " is running.");
-  } else {
+  } else if(trainNumber == 10) {
+    console.log("Train #10 begins running at noon.");
+  }
+  else {
       console.log("Train #" + trainNumber + " is not operational.");
   }
 }
