27b56dd89ffc6c938111dd05c8705e1ae2b8bc67
dastangoo <hamed.dastangoo@gmail.com>
2017-12-24 12:18:21 +0330

    Built-In's Bluff

---
+++ example02.js
@@ -0,0 +1,14 @@
+// alert("Alert! Alert! Last call to board Train #4!")
+// var val = confirm("Dude. Are you sure you wanna ride #8? Just saying.");
+// console.log(val);
+// var val = prompt("What soul hath allowed the canines to exit?")
+// console.log(val);
+
+var userName = prompt("Yo passenger! What's your name?");
+console.log(userName);
+var val = confirm("Are you sure your name is " + userName + "?");
+console.log(val);
+
+if (val) {
+  console.log("Hooray!");
+}
--- trains.js
+++ trains.js
@@ -1,28 +1,29 @@
-var trainNumber = 1;
-var trainsOperational = 8;
-var totalTrains = 12;
-var dayOfWeek = "Sunday";
+// var trainNumber = 1;
+// var trainsOperational = 8;
+// var totalTrains = 12;
+// var dayOfWeek = "Sunday";
+// 
+// 
+// if (trainsOperational > 0) {
+//   if (trainsOperational == totalTrains) {
+//     console.log("All trains are running at the JavaScript Express!");    
+//   } else {
+//     for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
+//       if (trainNumber <= trainsOperational && trainNumber !=3) {
+//         console.log("Train #" + trainNumber + " is running.");
+//       } else if(trainNumber == 10 || trainNumber == 12) {
+//         console.log("Train #" + trainNumber + " begins running at noon.");
+//       } else if(trainNumber == 3 && dayOfWeek == 'Sunday') {
+//         console.log("Train #3 is running.");
+//       } else {
+//           console.log("Train #" + trainNumber + " is not operational.");
+//       }
+//     }      
+//   }
+// } else {
+//   console.log("No trains are operational today. Bummer!");
+// }
 
-
-if (trainsOperational > 0) {
-  if (trainsOperational == totalTrains) {
-    console.log("All trains are running at the JavaScript Express!");    
-  } else {
-    for (var trainNumber = 1; trainNumber <= totalTrains; trainNumber++) {
-      if (trainNumber <= trainsOperational && trainNumber !=3) {
-        console.log("Train #" + trainNumber + " is running.");
-      } else if(trainNumber == 10 || trainNumber == 12) {
-        console.log("Train #" + trainNumber + " begins running at noon.");
-      } else if(trainNumber == 3 && dayOfWeek == 'Sunday') {
-        console.log("Train #3 is running.");
-      } else {
-          console.log("Train #" + trainNumber + " is not operational.");
-      }
-    }      
-  }
-} else {
-  console.log("No trains are operational today. Bummer!");
-}
 
 // for (var number = 5; number > 0; number--) {
 //   console.log(number);
@@ -36,3 +37,13 @@ if (trainsOperational > 0) {
 // for (var stoppedTrain = trainsOperational + 1; stoppedTrain <= totalTrains; stoppedTrain++) {
 //   console.log("Train #" + stoppedTrain + " is not operational.");
 // }
+
+
+var gotName = false;
+while (gotName == false) {
+  var userName = prompt("Yo passenger! What's your name?");
+  if (confirm("Are you sure your name is " + userName + "?")) {
+    alert("'Sup " + userName + "!");
+    gotName = true;
+  }
+}
